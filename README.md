170dc061acdc1b77f673f38ef60d1070f84281fa
sabina Akter <aktersabina122@gmail.com>
2022-12-27 19:49:17 -0500

    list of AWS Services

---
+++ README.md
@@ -0,0 +1,2 @@
+# Sample_Project on List
+#how to add, remove and reverse no from a list
---
+++ list.py
@@ -0,0 +1,42 @@
+
+# Create empty list
+
+aws_list = []
+
+#insert items into list
+
+aws_list.append("Amazon EC2")
+aws_list.append("Amazon RDS")
+aws_list.append("Amazon S3")
+aws_list.append("Amazon IAM")
+aws_list.append("Amazon EBS")
+aws_list.append("Amazon Lambda")
+aws_list.append("Amazon EFS")
+aws_list.append("Amazon CloudFront")
+aws_list.append("Amazon SNS")
+aws_list.append("Amazon VPC")
+
+
+#print list and length of list
+print(aws_list)
+print(f"There are {len(aws_list)} items in this list")
+
+#delete items from list
+del aws_list[3]
+aws_list.remove("Amazon SNS")
+
+#print updated list and length of list
+print(aws_list)
+print(f"There are {len(aws_list)} items in this list")
+
+#Create another list and copy
+aws_list_2 = []
+aws_list_2 = aws_list.copy()
+print("aws_list_2 =", aws_list)
+
+
+#printing index value
+indexvalue = aws_list_2[2:6]
+print("The index values are" , indexvalue)
+
+
